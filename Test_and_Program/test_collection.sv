a64f48d6c5c13c2a7ae22285bde7f01a640bc595
livebinary <livebinary619@gmail.com>
2020-08-23 15:20:15 +0530

    Coments added

--- Test_and_Program/test_collection.sv
+++ Test_and_Program/test_collection.sv
@@ -1,16 +1,19 @@
 import uvm_pkg::*;
 `include "uvm_macros.svh"
 
-class base_test extends uvm_test;
-`uvm_component_utils(base_test)
+class base_test extends uvm_test; //extend the base_test class from uvm_test
+`uvm_component_utils(base_test) // UVM factory registration
 
+// Constructor
 function new(string name , uvm_component parent);
     super.new(name , parent);
 endfunction
-
+// Start of Simulation Phase 
 virtual function void start_of_simulation_phase(uvm_phase phase);
+    `uvm_info(get_name(), "Entered Simulation Phase", UVM_NONE)
     super.start_of_simulation_phase(phase);
     uvm_root::get().print_topology();
+    `uvm_info(get_name(), "Exiting Simulation Phase", UVM_NONE)
 endfunction: start_of_simulation_phase
 
 
